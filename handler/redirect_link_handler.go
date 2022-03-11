bcfa1d4877d371b255bab7338517dbf3927541c3
Danil Andreyev <rusdteam@gmail.com>
2022-03-11 22:55:50 +0300

    Shortler

--- handler/redirect_link_handler.go
+++ handler/redirect_link_handler.go
@@ -1,27 +1,27 @@
 package handler
 
 import (
-    "github.com/gin-gonic/gin"
-    "net/http"
-    "shortler/db/dao"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"shortler/db/dao"
 )
 
 func RedirectLinkHandler(c *gin.Context) {
-    token := c.Param("token")
-    if token == "" {
-        c.JSON(404, gin.H{"error": "token_not_found"})
-        return
-    }
+	token := c.Param("token")
+	if token == "" {
+		c.JSON(404, gin.H{"error": "token_not_found"})
+		return
+	}
 
-    clientIP := c.Request.Header.Get("X-Forwarded-For")
+	//clientIP := c.Request.Header.Get("X-Forwarded-For")
 
-    linkDAO := dao.LinkDAO{}
-    link := linkDAO.GetByShort(token)
+	linkDAO := dao.LinkDAO{}
+	link := linkDAO.GetByShort(token)
 
-    if link == nil {
-        c.JSON(404, gin.H{"error": "token_not_found"})
-        return
-    }
+	if link == nil {
+		c.JSON(404, gin.H{"error": "token_not_found"})
+		return
+	}
 
-    c.Redirect(http.StatusFound, link.Link)
+	c.Redirect(http.StatusFound, link.Link)
 }
